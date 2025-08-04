3a1fdca0a29965435d9b555360b33284e8d1e363
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-04 13:10:39 -0400

    Update preinstall.ps1

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -1,4 +1,7 @@
-# Runs before the choco package is installed
-$INSTALL_DIR =  Join-Path $PSScriptRoot 'installer'
+# Runs before the choco package is installed 
 
+# Installation dir location
+$INSTALL_DIR = Join-Path $PSScriptRoot 'installer'
+
+Start-Process "msiexec.exe" -Wait -ArgumentList "/i $INSTALL_DIR\msodbcsql.msi /qn /norestart"
 gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
