dca1488abed212e6176928f9904ec25e8485a994
lb30 <39345501+lb30@users.noreply.github.com>
2025-08-06 10:49:45 -0400

    Update preinstall.ps1

--- packages/solidworks-coecis/tools/preinstall.ps1
+++ packages/solidworks-coecis/tools/preinstall.ps1
@@ -4,4 +4,6 @@
 $INSTALL_DIR = Join-Path $PSScriptRoot 'installer'
 
 Start-Process "msiexec.exe" -Wait -ArgumentList "/i $INSTALL_DIR\msodbcsql.msi /qn /norestart"
+Start-Process "$INSTALL_DIR\MicrosoftEdgeWebview2Setup.exe" -ArgumentList "/quiet /norestart" -Wait
+
 gci -Path $INSTALL_DIR\Microsoft* -Filter *.exe -Recurse | % {Start-Process $_.FullName -ArgumentList "/q /norestart" -wait -Verbose}
